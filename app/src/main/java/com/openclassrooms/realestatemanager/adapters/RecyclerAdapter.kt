85055479f35523df86f91feeed2c4804c767a17d
Julien Gacon <gaconjulien@gmail.com>
2019-08-19 19:35:12 +0200

    Add DAO to recyclerview

--- app/src/main/java/com/openclassrooms/realestatemanager/adapters/RecyclerAdapter.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/adapters/RecyclerAdapter.kt
@@ -1,9 +1,6 @@
 package com.openclassrooms.realestatemanager.adapters
 
-import android.content.res.Configuration
-import android.content.res.Resources
 import android.graphics.Color
-import android.text.TextUtils.replace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,11 +11,11 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import com.openclassrooms.realestatemanager.R
-import com.openclassrooms.realestatemanager.activities.MainPlaceActivity
-import com.openclassrooms.realestatemanager.fragments.DetailListFragment
-import java.text.FieldPosition
+import com.openclassrooms.realestatemanager.models.Place
 
-class RecyclerAdapter constructor(val mItemClickListener:ItemClickListener) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter constructor(private val mItemClickListener: ItemClickListener,
+                                  private var places: List<Place>)
+    : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     private val titles = arrayOf("House",
             "Flat", "House", "Duplex",
@@ -41,7 +38,7 @@ class RecyclerAdapter constructor(val mItemClickListener:ItemClickListener) : Re
             R.drawable.android_image_1, R.drawable.android_image_1,
             R.drawable.android_image_1)
 
-    var cardPosition: Int = 0
+    private var cardPosition: Int = 0
 
     interface ItemClickListener {
         fun onItemClick(position: Int)
@@ -54,17 +51,26 @@ class RecyclerAdapter constructor(val mItemClickListener:ItemClickListener) : Re
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return places.size
+    }
+
+    private fun getPlace(position: Int): Place {
+        return this.places[position]
+    }
+
+    fun updateData(places: List<Place>) {
+        this.places = places
+        this.notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
         val context = viewHolder.itemView.context // get context for bg color
 
-        viewHolder.itemTitle.text = titles[i]
+        viewHolder.itemTitle.text = getPlace(i).placeTitle
         viewHolder.itemLocalisation.text = localisation[i]
         viewHolder.itemImage.setImageResource(images[i])
-        viewHolder.itemPrice.text = price[i]
-        // on item selected
+        viewHolder.itemPrice.text = getPlace(i).placePrice.toString()
+        // On item selected
         viewHolder.itemView.setOnClickListener { v: View ->
             val position: Int = viewHolder.adapterPosition
             cardPosition = viewHolder.adapterPosition
